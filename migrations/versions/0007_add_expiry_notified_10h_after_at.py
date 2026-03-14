8f1bd3eadb9cb5e8d4a34fbf2b0dee699b912c99
gotsta1 <a3unnn@gmail.com>
2026-03-14 16:41:09 +0500

    Shorten alembic revision id for 10h notification

--- migrations/versions/0007_add_expiry_notified_10h_after_at.py
+++ migrations/versions/0007_add_expiry_notified_10h_after_at.py
@@ -1,6 +1,6 @@
 """add expiry_notified_10h_after_at to entitlements
 
-Revision ID: 0007_add_expiry_notified_10h_after_at
+Revision ID: 0007
 Revises: 0006
 Create Date: 2026-03-14
 """
@@ -11,7 +11,7 @@ from alembic import op
 import sqlalchemy as sa
 
 
-revision = "0007_add_expiry_notified_10h_after_at"
+revision = "0007"
 down_revision = "0006"
 branch_labels = None
 depends_on = None
